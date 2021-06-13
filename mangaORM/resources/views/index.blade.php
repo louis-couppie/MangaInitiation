b8bbe9758a9a45ab71f2206275bf2875f0767971
louis-couppie <louis.couppie@etu.univ-lyon1.fr>
2021-06-13 13:38:11 +0200

    Section 6 : premiers essais avec Eloquent

---
+++ mangaORM/resources/views/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layout.layout')
+
+@section('titrePage')
+    Liste des mangas
+@endsection
+
+@section('titreItem')
+    <h1>Tous les mangas :</h1>
+@endsection
+
+@section('contenu')
+
+    @if(session()->has('info'))
+        <div class="card text-white bg-success mb-3" style="max-wdith: 18rem;">
+            <div class="card-body">
+                <p class="card-text">{{ session('info') }}</p>
+            </div>
+        </div>
+    @endif
+
+    <div class="card">
+        <header class="card-header">
+            <h5 class="card-header-title">Nous avons sélectionné pour vous :</h5>
+        </header>
+        <div class="card-header">
+            <div class="content">
+                <table class="table-is-hoverable">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Titre</th>
+                            <th>Genre</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    @foreach($mangas as $manga)
+                        <tr>
+                            <td>{{ $manga->id }}</td>
+                            <td><strong>{{ $manga->titre }}</strong></td>
+                            <td>{{ $manga->genre }}</td>
+                            <td><a class="btn btn-primary" href="{{ route('mangas.show', $manga->id) }}">Voir</a></td>
+                            <td>
+                                <form action="{{ route('mangas.destroy', $manga->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger" type="submit">Supprimer</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- mangaORM/routes/web.php
+++ mangaORM/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Manga;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('mangas',App\Http\Controllers\MangaController::class);
